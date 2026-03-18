5ec5fa661f5d3a87b9bcce3c718f8eba9b5f2ac7
Haley Harper <haleydawn05@icloud.com>
2026-03-18 14:20:39 -0500

    Add output formatting to hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
+cout << "============================" << endl;
+cout << "       Program Output       " << endl;
+cout << "============================" << endl;
+  
 cout << “FEATURE 2 VERSION AND MAIN!” << endl;
 cout << "Hi, Haley" << endl;
+  
+cout << "============================" << endl;
+  
 return 0;
 }
